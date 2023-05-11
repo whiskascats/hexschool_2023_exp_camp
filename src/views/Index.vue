a8c33a87e1b28dcaac8b5600649de7ef4987b56c
Whiskas <toby010203222@gmail.com>
2023-05-11 23:23:47 +0800

    環境變數設定

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,4 +3,4 @@
     
   </div>
 </template>
-<script></script>
+<script lang="ts"></script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': '/src' // 將 '@' 別名指向 '/src' 目錄
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  
+  return defineConfig({
+    base: process.env.VITE_GITHUB_URL,
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': '/src' // 將 '@' 別名指向 '/src' 目錄
+      }
+    },
+    server: {
+      port: 8080
     }
-  },
-  server: {
-    port: 8080
-  }
-})
+  })  
+}
